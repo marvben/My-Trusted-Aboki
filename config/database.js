9fe60b0bb823b1c56f345bda39f458bb3fa7d5fe
unknown <benluvforu@gmail.com>
2022-09-26 01:23:46 +0100

    add CK Editor to replace texarea on creating and editing post body

--- config/database.js
+++ config/database.js
@@ -1,8 +1,7 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-const url = process.env.DB_STRING
-
+const url = process.env.DB_STRING;
 
 main().catch((err) => console.log(err));
 
@@ -10,17 +9,33 @@ async function main() {
   await mongoose.connect(url);
 }
 
+// Creates simple schema for a users.
+// const userSchema = new mongoose.Schema({
+//   first_name: { type: String, default: null },
+//   last_name: { type: String, default: null },
+//   email: { type: String, unique: true },
+//   password: { type: String },
+//   token: { type: String },
+// });
 
+// module.exports = mongoose.model("user", userSchema);
 
 // Creates simple schema for a posts.
-const customValidation= {type: String, trim: true,required: true};
+const customValidation = {
+  type: String,
+  trim: true,
+  required: true,
+  default: null,
+};
 
 const postSchema = new mongoose.Schema({
   title: customValidation,
   content: customValidation,
-  postType:customValidation, 
-  date:Date,
-  comments:[{date:Date, postComment:customValidation, username:customValidation}]
+  postType: customValidation,
+  date: Date,
+  comments: [
+    { date: Date, postComment: customValidation, username: customValidation },
+  ],
 });
 
 const Post = mongoose.model("Post", postSchema);
